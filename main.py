fb5646cb54bd3cd75f9e6721b09642e50b0db349
caVenikk <sqqvt1@gmail.com>
2022-02-08 11:12:14 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import pygame
-from pygame import Rect
-import pygame_gui
-from pygame_gui.elements.ui_text_entry_line import UITextEntryLine
-
-import sys
 import random
-import datetime
+import sys
+
+import pygame
+import pygame_gui
+from pygame import Rect
+from pygame_gui.elements.ui_text_entry_line import UITextEntryLine
 
 import parser
 
@@ -253,7 +252,7 @@ class Hero:
                     self.rect.y = 470 - self.rect.height
                     self.currentFrame = 5
                     gameState.alive = False
-                    SaveScore()
+                    parser.save_score(name, gameState)
 
         if self.Intersects(pterodactyl) and not self.godmode:
             self.hp -= 3
@@ -262,7 +261,7 @@ class Hero:
                 self.rect.y = 470 - self.rect.height
                 self.currentFrame = 5
                 gameState.alive = False
-                SaveScore()
+                parser.save_score(name, gameState)
 
         for bonus in bonuses:
             if isinstance(bonus, HealBonus) and self.Intersects(bonus):
@@ -358,17 +357,6 @@ class GodModeBonus(Bonus):
         self.rect.y = 470 - self.rect.height
 
 
-def SaveScore():
-    now = datetime.datetime.now()
-    f = open('score.txt', 'r')
-    lines = f.readlines()
-    lines.append(f'{name} {gameState.count} {now.day}.{now.month}.{now.year} {now.hour}:{now.minute}\n')
-    f.close()
-    f = open('score.txt', 'w')
-    f.writelines(lines)
-    f.close()
-
-
 def draw(screen, gamestate):
     global text_hp_value
     if not gamestate.alive:
@@ -436,6 +424,109 @@ def update(gameState):
         gameState.Update()
 
 
+def mainloop():
+    while True:
+        clock.tick(120)
+        screen.fill((248, 248, 248))
+        update(gameState)
+        draw(screen, gameState)
+        pygame.display.update()
+
+
+def button(screen, position, text):
+    font = pygame.font.Font("Hardpixel.ttf", 30)
+    text_render = font.render(text, True, (83, 83, 83))
+    text_rect = text_render.get_rect()
+    text_rect.x, text_rect.y = position
+    pygame.draw.line(screen, (150, 150, 150), (text_rect.x, text_rect.y), (text_rect.x + 125, text_rect.y), 5)
+    pygame.draw.line(screen, (150, 150, 150), (text_rect.x, text_rect.y - 2), (text_rect.x, text_rect.y + text_rect.h),
+                     5)
+    pygame.draw.line(screen, (50, 50, 50), (text_rect.x, text_rect.y + text_rect.h),
+                     (text_rect.x + 125, text_rect.y + text_rect.h), 5)
+    pygame.draw.line(screen, (50, 50, 50), (text_rect.x + 125, text_rect.y + text_rect.h),
+                     [text_rect.x + 125, text_rect.y], 5)
+    pygame.draw.rect(screen, (170, 170, 170), (text_rect.x, text_rect.y, 125, text_rect.h))
+    return screen.blit(text_render, (text_rect.x, text_rect.y))
+
+
+def scoreboard_menu():
+    global screen, clock, cacti, pterodactyl, hero, gameState, cloud, background, bonuses, font, fontdead, text_hp, scoreboard_bg_img
+    screen.blit(scoreboard_bg_img, (0, 0))
+    b1 = button(screen, (10, 120), "Back")
+    data = parser.get_scores_from_file('score.txt')
+    top5 = font.render(f"Top 5 best players", True, [60, 60, 60])
+    players = []
+    has_records = False
+    if len(data) <= 0:
+        players.append(font.render(f"So far, no one has set a record. Be the first!", True, [83, 83, 83]))
+    else:
+        for i in range(len(data)):
+            players.append(
+                font.render(f"{i + 1}. {data[i]['name']} — {data[i]['score']} ({data[i]['date']} {data[i]['time']})",
+                            True, [83, 83, 83]))
+            has_records = True
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    menu()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if b1.collidepoint(pygame.mouse.get_pos()):
+                    menu()
+        screen.blit(top5, (280, 125))
+        y_pos = 175
+        for player in players:
+            if has_records:
+                screen.blit(player, (230, y_pos))
+            else:
+                screen.blit(player, (165, y_pos))
+            y_pos += 50
+        pygame.display.update()
+
+
+def menu():
+    global screen, clock, cacti, pterodactyl, hero, gameState, cloud, background, bonuses, font, fontdead, text_hp, menu_bg_img, name
+    screen.blit(menu_bg_img, (0, 0))
+    b1 = button(screen, (350, 400), "Quit")
+    b2 = button(screen, (350, 200), "Start")
+    b3 = button(screen, (350, 300), "Score")
+    enter_name = font.render(f"Enter your name and press Enter key", True, [83, 83, 83])
+    manager = pygame_gui.UIManager((800, 500))
+    text_input = UITextEntryLine(relative_rect=Rect(310, 160, 200, 20), manager=manager)
+    while True:
+        screen.blit(enter_name, (210, 135))
+        time_delta = clock.tick(60) / 1000.0
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE and not gameState.alive:
+                    pygame.quit()
+                if event.key == pygame.K_ESCAPE and gameState.alive:
+                    mainloop()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if b1.collidepoint(pygame.mouse.get_pos()):
+                    pygame.quit()
+                elif b2.collidepoint(pygame.mouse.get_pos()):
+                    mainloop()
+                elif b3.collidepoint(pygame.mouse.get_pos()):
+                    scoreboard_menu()
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
+                    if event.ui_element == text_input:
+                        name = text_input.get_text()
+                        name_successful = font.render(f"Have a good game, {name}!", True, [130, 130, 130])
+                        screen.blit(name_successful, (10, 470))
+            manager.process_events(event)
+        manager.update(time_delta)
+        screen.blit(screen, (0, 0))
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
 pygame.init()
 pygame.display.set_caption('Chrome Dino')
 screen = pygame.display.set_mode((800, 500))
@@ -475,111 +566,5 @@ fontdead = pygame.font.Font("Hardpixel.ttf", 50)
 text_hp = font.render("Health", True, [161, 155, 155])
 text_hp_value = font.render("", True, [0, 0, 0])
 
-
-def mainloop():
-    while True:
-        clock.tick(120)
-        screen.fill((248, 248, 248))
-        update(gameState)
-        draw(screen, gameState)
-        pygame.display.update()
-
-
-def button(screen, position, text):
-    font = pygame.font.Font("Hardpixel.ttf", 30)
-    text_render = font.render(text, True, (83, 83, 83))
-    text_rect = text_render.get_rect()
-    text_rect.x, text_rect.y = position
-    pygame.draw.line(screen, (150, 150, 150), (text_rect.x, text_rect.y), (text_rect.x + 125, text_rect.y), 5)
-    pygame.draw.line(screen, (150, 150, 150), (text_rect.x, text_rect.y - 2), (text_rect.x, text_rect.y + text_rect.h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (text_rect.x, text_rect.y + text_rect.h), (text_rect.x + 125, text_rect.y + text_rect.h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (text_rect.x + 125, text_rect.y + text_rect.h), [text_rect.x + 125, text_rect.y], 5)
-    pygame.draw.rect(screen, (170, 170, 170), (text_rect.x, text_rect.y, 125, text_rect.h))
-    return screen.blit(text_render, (text_rect.x, text_rect.y))
-
-
-def start():
-    mainloop()
-
-
-def make_scoreboard():
-    pass
-
-
-def scoreboard_menu():
-    global screen, clock, cacti, pterodactyl, hero, gameState, cloud, background, bonuses, font, fontdead, text_hp, scoreboard_bg_img
-    screen.blit(scoreboard_bg_img, (0, 0))
-    b1 = button(screen, (10, 120), "Back")
-    data = parser.get_scores_from_file('score.txt')
-    top5 = font.render(f"Top 5 best players", True, [60, 60, 60])
-    players = []
-    if 0 < len(data) < 5:
-        for i in range(len(data)):
-            players.append(font.render(f"{data[i]['name']} — {data[i]['score']} ({data[i]['date']} {data[i]['time']})", True, [83, 83, 83]))
-    elif len(data) <= 0:
-        players.append(font.render(f"So far, no one has set a record. Be the first!", True, [83, 83, 83]))
-    else:
-        for i in range(5):
-            players.append(font.render(f"{data[i]['name']} — {data[i]['score']} ({data[i]['date']} {data[i]['time']})", True, [83, 83, 83]))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    menu()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if b1.collidepoint(pygame.mouse.get_pos()):
-                    menu()
-        screen.blit(top5, (280, 125))
-        y_pos = 175
-        for player in players:
-            screen.blit(player, (230, y_pos))
-            y_pos += 50
-        pygame.display.update()
-
-
-def menu():
-    global screen, clock, cacti, pterodactyl, hero, gameState, cloud, background, bonuses, font, fontdead, text_hp, menu_bg_img, name
-    screen.blit(menu_bg_img, (0, 0))
-    b1 = button(screen, (350, 400), "Quit")
-    b2 = button(screen, (350, 200), "Start")
-    b3 = button(screen, (350, 300), "Score")
-    enter_name = font.render(f"Enter your name and press Enter key", True, [83, 83, 83])
-    manager = pygame_gui.UIManager((800, 500))
-    text_input = UITextEntryLine(relative_rect=Rect(310, 160, 200, 20), manager=manager)
-    while True:
-        screen.blit(enter_name, (210, 135))
-        time_delta = clock.tick(60) / 1000.0
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE and not gameState.alive:
-                    pygame.quit()
-                if event.key == pygame.K_ESCAPE and gameState.alive:
-                    start()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if b1.collidepoint(pygame.mouse.get_pos()):
-                    pygame.quit()
-                elif b2.collidepoint(pygame.mouse.get_pos()):
-                    start()
-                elif b3.collidepoint(pygame.mouse.get_pos()):
-                    scoreboard_menu()
-            if event.type == pygame.USEREVENT:
-                if event.user_type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
-                    if event.ui_element == text_input:
-                        name = text_input.get_text()
-                        name_successful = font.render(f"Have a good game, {name}!", True, [130, 130, 130])
-                        screen.blit(name_successful, (10, 470))
-            manager.process_events(event)
-        manager.update(time_delta)
-        screen.blit(screen, (0, 0))
-        manager.draw_ui(screen)
-        pygame.display.update()
-
-
 if __name__ == '__main__':
     menu()
-
--- parser.py
+++ parser.py
@@ -1,4 +1,20 @@
+import datetime
+
+
+def save_score(name, gameState):
+    now = datetime.datetime.now()
+    f = open('score.txt', 'r')
+    lines = f.readlines()
+    lines.append(
+        f'{name} {gameState.count} {"0" if len(str(now.day)) == 1 else ""}{now.day}.{"0" if len(str(now.month)) == 1 else ""}{now.month}.{now.year} {now.hour}:{now.minute}\n')
+    f.close()
+    f = open('score.txt', 'w')
+    f.writelines(lines)
+    f.close()
+
+
 def get_scores_from_file(filename):
     with open(filename, 'r') as file:
         d = [line.split() for line in file.readlines()]
-        return sorted([{'name': r[0], 'score': int(r[1]), 'date': r[2], 'time': r[3]} for r in d], key=lambda item: item['score'], reverse=True)
+        return sorted([{'name': r[0], 'score': int(r[1]), 'date': r[2], 'time': r[3]} for r in d],
+                      key=lambda item: item['score'], reverse=True)
